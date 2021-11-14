132d988c111de958585c714afc2a0018f65e7801
petrikovandrey <aapetrikov@gmail.com>
2021-11-14 16:13:52 +0200

    setinterval

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,20 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function startMain() {
   const sizeBoard = 4;
-  //   const random = getRandomInt(Math.pow(sizeBoard, 2));
+  const randomN = getRandomInt(sizeBoard ** 2);
   let n = 0;
 
   for (let i = 0; i < sizeBoard; i += 1) {
-    // const img = '#';
+    let imgGoblin = '#';
     n += 1;
     for (let b = 0; b < sizeBoard; b += 1) {
+      imgGoblin = (n === randomN) ? './img/goblin.png' : '#';
+
       const div = `<div class = "w" id=${String(n)}>
-              <a href="./img/netology.svg">
+              <a href=${imgGoblin}>
               </div>`;
       document.section.append(div);
       n += 1;
@@ -16,8 +22,11 @@ function startMain() {
   }
 }
 
-// function getRandomInt(max) {
-//   return Math.floor(Math.random() * max);
-// }
-
+function updateBord() {
+  setInterval(() => {
+    const randomN = getRandomInt(16);
+    document.getElementById(String(randomN)).href = './img/goblin.png';
+  }, 2);
+}
 startMain();
+updateBord();
