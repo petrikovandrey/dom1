e670373bb0bfa816b165b789d8c7a662b2b7e44d
petrikovandrey <aapetrikov@gmail.com>
2021-11-14 15:32:51 +0200

    eslint = done

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,23 @@
 function startMain() {
+  const sizeBoard = 4;
+  //   const random = getRandomInt(Math.pow(sizeBoard, 2));
+  let n = 0;
 
-    const sizeBoard = 4;
-    const random = getRandomInt(Math.pow(sizeBoard, 2));
-    let n = 0;
-
-    for (let i = 0; i < sizeBoard; i++) {
-        let img = "#";
-        n++;
-        for (let b = 0; b < sizeBoard; b++) {
-            if (random === n) {
-                img = "./img/netology.svg";
-            }
-            let div = `<div class = "w" id=${String(n)}>
+  for (let i = 0; i < sizeBoard; i += 1) {
+    // const img = '#';
+    n += 1;
+    for (let b = 0; b < sizeBoard; b += 1) {
+      const div = `<div class = "w" id=${String(n)}>
               <a href="./img/netology.svg">
               </div>`;
-            document.section.append(div);
-            n++;
-        }
-    };
+      document.section.append(div);
+      n += 1;
+    }
+  }
 }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * max);
+// }
+
+startMain();
