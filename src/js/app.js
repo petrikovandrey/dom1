831f4f1cf811407eb72ac1ee1c971fd9114552f1
Andrey <55155796+petrikovandrey@users.noreply.github.com>
2021-11-17 23:52:04 +0200

    Update app.js
    done

--- src/js/app.js
+++ src/js/app.js
@@ -3,31 +3,41 @@ function getRandomInt(max) {
 }
 
 function startMain() {
+  const content = document.querySelector('section');
   const sizeBoard = 4;
   const randomN = getRandomInt(sizeBoard ** 2);
   let n = 0;
 
   for (let i = 0; i < sizeBoard; i += 1) {
-    let imgGoblin = '#';
-    n += 1;
+    let imgGoblin = '<img class="center" src=https://github.com/netology-code/ahj-homeworks/raw/simplification/dom/pic/goblin.png>';
     for (let b = 0; b < sizeBoard; b += 1) {
-      imgGoblin = (n === randomN) ? './img/goblin.png' : '#';
+      imgGoblin = (n === randomN) ? imgGoblin : '';
 
       const div = `<div class = "w" id=${String(n)}>
-              <a href=${imgGoblin}>
+            ${imgGoblin}
               </div>`;
-      document.section.append(div);
+      content.insertAdjacentHTML('afterbegin', div);
       n += 1;
     }
   }
+  updateBord();
 }
 
 function updateBord() {
   setInterval(() => {
     const randomN = getRandomInt(16);
-    document.getElementById(String(randomN)).href = './img/goblin.png';
-  }, 2);
+    const img = document.querySelector('.center');
+    if (img != null) {
+      while (randomN === img.parentNode.id) {
+        randomN = getRandomInt(16);
+      }
+
+      img.remove();
+      let div = document.getElementById(String(randomN));
+      div.insertAdjacentHTML('afterbegin', '<img class="center" src=https://github.com/netology-code/ahj-homeworks/raw/simplification/dom/pic/goblin.png>');
+    }
+  }, 200);
 }
 
 startMain();
-updateBord();
+
